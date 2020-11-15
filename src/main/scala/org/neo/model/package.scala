dc006a41cca847486f0b7a886e013d9b6a4e4eb1
Neo Lin <Neo Lin>
2020-11-14 19:54:52 -0500

    clean up

--- src/main/scala/org/neo/model/package.scala
+++ src/main/scala/org/neo/model/package.scala
@@ -5,6 +5,6 @@ import akka.actor.typed.ActorRef
 package object model {
   sealed trait Command
   final case class ServiceConfig(someKey: String = "Good Morning")
-  case object GetMessage extends Command
+  case object GetMessage
   final case class GetTypeMessage(replyTo: ActorRef[String]) extends Command
 }
